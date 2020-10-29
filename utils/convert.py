c39a56552080967afd90abba2dbce788e2784238
ybai62868 <ybai62868@gmail.com>
2020-10-29 19:39:57 +0800

    [Add] Add the project structure for deepsort

---
+++ utils/convert.py
@@ -0,0 +1,20 @@
+
+# changing the dict format to binary format for the net to read
+
+import os
+import torch
+import numpy as np
+
+
+
+model_path = './ckpt.t7'
+state_dict = torch.load(model_path)['net_dict']
+
+
+with open('ckpt.bin', 'wb') as f:
+    for key in state_dict.keys():
+        f.write(state_dict[key].cpu().numpy())
+
+
+
+
